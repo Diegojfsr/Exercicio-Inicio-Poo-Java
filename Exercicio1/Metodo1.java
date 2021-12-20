0c38334637d989b68f18666d23d6675e2614f506
Diego Jefferson <diegojfsr@gmail.com>
2021-12-20 11:22:58 -0300

    Exercicio1

--- Exercicio1/Metodo1.java
+++ Exercicio1/Metodo1.java
@@ -12,23 +12,40 @@ public class Metodo1 {
         
 		for(int i= 0; i<100; i++) {
 			int x = (int) (Math.random()* range) + min;
-			y[i] = x;			
+			y[i] = x;		
+			
+			System.out.println(x);
 		}
 		
 		int contador = contarNegativos(y);
 		System.out.println("A quantidade de números negativos é " + contador);
+
+		int contador2 = contarPositivos(y);
+		System.out.println("A quantidade de números positivos é " + contador2);
 		
 		
 	}
-	
+
 	private static int contarNegativos(int[] y) {
 		int contador = 0;
-		for(int a: y) {
-			if(a<0) {
+		for(int b: y) {
+			if(b<0) {
 				contador++;
 			}
 		}
 		return contador;
 
     }
+	
+	private static int contarPositivos(int[] y) {
+		int contador2 = 0;
+		for(int a: y) {
+			if(a>0) {
+				contador2++;
+			}
+		}
+		return contador2;
+	}
+
+
 }
